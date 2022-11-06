7620f708e6eae63a4451157b5f9840c4344876dd
Pat Doyle <patdoyle@Pats-MacBook-Pro.local>
2022-11-06 17:20:08 -0600

    adding stuff

--- week2-assignment/hardhat.config.ts
+++ week2-assignment/hardhat.config.ts
@@ -1,8 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  paths: { tests: "test" },
 };
 
 export default config;
